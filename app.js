f11b0ee28360070b33e49d2a083db74d6296a5e1
Cosmas-Emeka <cosmasemeka11@gmail.com>
2023-07-25 01:37:13 +0100

    Preloader setup underway

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const list = document.querySelectorAll(".list");
+const loader = document.getElementById("preloader");
 
 function activeLink() {
     list.forEach((item) =>
@@ -8,3 +9,9 @@ function activeLink() {
 
 list.forEach((item) =>
 item.addEventListener("click", activeLink));
+
+
+//
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+})

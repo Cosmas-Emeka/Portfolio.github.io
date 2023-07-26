846d519f2f42ec843ebfd5a5b15f6494d44efea7
Cosmas-Emeka <cosmasemeka11@gmail.com>
2023-07-26 15:33:50 +0100

    Added js intersectionObserver for home page animation

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+//declaring the variables(DOM)
 const list = document.querySelectorAll(".list");
 const loader = document.getElementById("preloader");
 
+
+
+//fx for bottom nav active links
 function activeLink() {
     list.forEach((item) =>
     item.classList.remove("active"));
@@ -11,7 +15,25 @@ list.forEach((item) =>
 item.addEventListener("click", activeLink));
 
 
-//
+
+//preloader fx
 window.addEventListener("load", function(){
     loader.style.display = "none";
 })
+
+
+
+//
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show")
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
